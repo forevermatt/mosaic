560390b9b5be7ce7165898e8252d7986280302dc
forevermatt <forevermatt@users.noreply.github.com>
2015-06-25 15:19:04 -0500

    Limit internal use of source images to 200px wide.

--- MosaicMaker.php
+++ MosaicMaker.php
@@ -25,7 +25,8 @@ class MosaicMaker
         foreach ($pathsToSourceImages as $pathToSourceImage) {
             $sourceImages[] = new Image(
                 $pathToSourceImage,
-                $guideImageAspectRatio
+                $guideImageAspectRatio,
+                200
             );
         }
         
