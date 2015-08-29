66f602121ffcf7d7fd24e94e6beb479cafd6254b
forevermatt <forevermatt@users.noreply.github.com>
2015-08-29 17:16:16 -0400

    Remove debug code.

--- src/SliceGrid.php
+++ src/SliceGrid.php
@@ -38,12 +38,6 @@ class SliceGrid
         $guideImageAspectRatio,
         $desiredSliceAspectRatio = self::DEFAULT_DESIRED_ASPECT_RATIO
     ) {
-        //echo PHP_EOL;
-        //echo 'maxSlices: ' . $maxSlices . PHP_EOL;
-        //echo 'minSlices: ' . $minSlices . PHP_EOL;
-        //echo 'guideImageAspectRatio: ' . $guideImageAspectRatio . PHP_EOL;
-        //echo 'desiredSliceAspectRatio: ' . $desiredSliceAspectRatio . PHP_EOL;
-        
         if ($maxSlices < $minSlices) {
             throw new \InvalidArgumentException(
                 'The maximum number of slices must not be less than the '
@@ -61,27 +55,6 @@ class SliceGrid
             )
         );
         
-        //echo 'sqrt('
-        //   .     '($minSlices / $guideImageAspectRatio) / $desiredSliceAspectRatio'
-        //   . ') * $guideImageAspectRatio' . PHP_EOL;
-        //echo 'sqrt('
-        //   .     '(' . $minSlices . ' / ' . $guideImageAspectRatio . ') / ' . $desiredSliceAspectRatio
-        //   . ') * ' . $guideImageAspectRatio . PHP_EOL;
-        //echo 'sqrt('
-        //   .     '(' . $minSlices / $guideImageAspectRatio . ') / ' . $desiredSliceAspectRatio
-        //   . ') * ' . $guideImageAspectRatio . PHP_EOL;
-        //echo 'sqrt('
-        //   .     '(' . $minSlices / $guideImageAspectRatio . ') / ' . $desiredSliceAspectRatio
-        //   . ') * ' . $guideImageAspectRatio . PHP_EOL;
-        //echo 'sqrt('
-        //   .     ( $minSlices / $guideImageAspectRatio ) / $desiredSliceAspectRatio
-        //   . ') * ' . $guideImageAspectRatio . PHP_EOL;
-        //echo sqrt( ( $minSlices / $guideImageAspectRatio ) / $desiredSliceAspectRatio )
-        //   . ' * ' . $guideImageAspectRatio . PHP_EOL;
-        //echo sqrt( ( $minSlices / $guideImageAspectRatio ) / $desiredSliceAspectRatio )
-        //     * $guideImageAspectRatio . PHP_EOL;
-        //echo 'minNumColumns: ' . $minNumColumns . PHP_EOL;
-        
         $maxNumColumns = (int)floor(
             round(
                 sqrt(
@@ -91,23 +64,13 @@ class SliceGrid
             )
         );
         
-        //echo 'maxNumColumns: ' . $maxNumColumns . PHP_EOL;
-        
         $numColumns = null;
         $numRows = null;
         $bestAccuracy = null;
         
-        //echo 'x = $maxNumColumns' . PHP_EOL;
-        //echo 'x = ' . $maxNumColumns . PHP_EOL;
-        //echo '(x >= minNumColumns): ' . var_export(floor($maxNumColumns) >= $minNumColumns, true) . PHP_EOL;
-        //echo '(' . floor($maxNumColumns) . ' >= ' . $minNumColumns . '): ' . var_export(floor($maxNumColumns) >= $minNumColumns, true) . PHP_EOL;
-        //echo '(' . (int)floor($maxNumColumns) . ' >= ' . (int)$minNumColumns . '): ' . var_export(((int)floor($maxNumColumns)) >= ((int)$minNumColumns), true) . PHP_EOL;
-        
         for ($x = $maxNumColumns; $x >= $minNumColumns; $x--) {
             
             $y = $x * $desiredSliceAspectRatio / $guideImageAspectRatio;
-            
-            //echo 'y: ' . $y . PHP_EOL;
             
             // Figure out how far away from a whole number y was, rounding
             // slightly to correct for inaccuracies of floating point numbers.
@@ -116,8 +79,6 @@ class SliceGrid
                 self::FLOAT_MAX_ACCURATE_DECIMAL_DIGITS
             );
             
-            //echo 'yLeftoverFraction: ' . $yLeftoverFraction . PHP_EOL;
-            
             // Figure out how close we were (either above or below) to having
             // the number of rows (y) come out as a whole number.
             $accuracyOfFit = min(
@@ -125,20 +86,13 @@ class SliceGrid
                 abs(1 - $yLeftoverFraction)
             );
             
-            //echo 'accuracyOfFit: ' . $accuracyOfFit . PHP_EOL;
-            
             if (($bestAccuracy === null) || ($accuracyOfFit < $bestAccuracy)) {
                 $bestAccuracy = $accuracyOfFit;
                 $numColumns = $x;
                 $numRows = round($y);
                 
-                //echo '(best fit so far)' . PHP_EOL;
-                
                 // If it was a perfect fit, stop trying to find a better fit.
                 if ($bestAccuracy === 0) {
-                    
-                    //echo '(prefect fit)' . PHP_EOL;
-                    
                     break;
                 }
             }
